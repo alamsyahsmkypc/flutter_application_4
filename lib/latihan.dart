af35598c1ea8e28193249a7f6fe204f486814831
alamsyahsmkypc <alamsyahfirdaus.ypc@gmail.com>
2025-09-02 07:54:52 +0700

    Menambah File Baru

---
+++ lib/latihan.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Latihan extends StatelessWidget {
+  const Latihan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
